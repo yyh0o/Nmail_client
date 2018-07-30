a86e38ec2b62f6137f3ee207bf5e690000f10f14
yyh <646167686@qq.com>
2018-07-31 00:44:57 +0800

    完成字符串网络传输和文件网络传输

--- client.c
+++ client.c
@@ -3,3 +3,32 @@
 //
 
 #include "client.h"
+
+
+
+int clientRun(){
+    int sockfd = new_connected_sock(SERV_IP, SERV_PORT);
+    char sendBuff[BUFFER_SIZE];
+    char rcevBuff[BUFFER_SIZE];
+    bzero(sendBuff,BUFFER_SIZE);
+    bzero(rcevBuff,BUFFER_SIZE);
+    
+    while (1) {
+        scanf("%s", sendBuff);//输入发送数据
+        fflush(stdin);//清除输入缓存
+
+        if (strcmp(sendBuff, "exit") == 0) {
+            mySendMsg(sockfd, "0", 0, 'x');
+            break;
+        }
+        if (mySendMsg(sockfd, sendBuff, sizeof(sendBuff), '1') == -1) {
+            perror("send");
+            exit(EXIT_FAILURE);
+        }
+        char* type;
+        myRecvMsg(sockfd, rcevBuff, &type);
+        printf("%s", rcevBuff);
+    }
+    close(sockfd);
+    exit(0);
+}
--- client.h
+++ client.h
@@ -5,4 +5,22 @@
 #ifndef NMAIL_CLIENT_CLIENT_H
 #define NMAIL_CLIENT_CLIENT_H
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mySocket.h"
+
+
+
+#define SERV_IP "127.0.0.1"
+#define SERV_PORT 23333
+#define TMP_BUFFER_SIZE 100
+
+int clientRun();
+
 #endif //NMAIL_CLIENT_CLIENT_H
